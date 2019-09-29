407ddde40739f02c597a495553b349e2b00fa96a
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-29 19:38:38 +0300

    Updates

---
+++ precache-manifest.ca8858a4c28c1e34ef0c49f87ca7e7e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b90a84064967c6696e00141f38eca8",
+    "url": "/form-validation-with-reactjs/index.html"
+  },
+  {
+    "revision": "18e5f8c7754008fbb079",
+    "url": "/form-validation-with-reactjs/static/css/main.1cde3033.chunk.css"
+  },
+  {
+    "revision": "c2980777208111d51b78",
+    "url": "/form-validation-with-reactjs/static/js/2.7c7c8ed3.chunk.js"
+  },
+  {
+    "revision": "18e5f8c7754008fbb079",
+    "url": "/form-validation-with-reactjs/static/js/main.2c931dbe.chunk.js"
+  },
+  {
+    "revision": "2f7aa31a09ac2b08c2dc",
+    "url": "/form-validation-with-reactjs/static/js/runtime-main.116492d7.js"
+  }
+]);
